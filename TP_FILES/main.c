9dc5522620d1e6a642b32a55eb7072197576aa3c
O-vide <o.viceconte@ludus-academie.com>
2021-06-16 17:48:01 +0200

    commit travail 2

--- TP_FILES/main.c
+++ TP_FILES/main.c
@@ -1,110 +1,153 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+
+#define WINDOW_WIDTH 724
+#define WINDOW_HEIGHT 480
+
+SDL_Window *pWindow=NULL;
+SDL_Renderer *pRenderer=NULL;
+SDL_Surface *pSurface=NULL;
+SDL_Texture *pTexture=NULL;
 
 typedef struct ElementListe{
-    char *chr_donnee;
+    //char *chr_donnee;
+    SDL_Texture *pIMG;
     struct ElementListe *p_suivant;
 }Element;
 
-typedef struct ListeChar{ //équivaut Liste_entier -----------
+typedef struct FileImg{
     Element *fl_debut;
     Element *fl_fin;
     int n_taille;
-}ListeChar;
-
-// Consulter le premier élément de la file
-#define file_donnee1(File) File->fl_debut->chr_donnee
-//ne le supprimera pas de la liste
-
-// Consulter le dernier élément de la file
-#define file_donnee2(File) File->fl_fin->chr_donnee
+}FileImg;
 
 
-void initFile(ListeChar* File); // initialisation des données à 0 ou NULL
-int insertElement(ListeChar * File , Element * courant, char *chr_donnee); // inseration d'un élément dans la file
-int suppElement(ListeChar * File);
-void afficheFile(ListeChar *File);
+void initFile(FileImg* File); // initialisation des données à 0 ou NULL
+int insertElement(FileImg * File , Element * courant, SDL_Texture *pTexture); // inseration d'un élément dans la file
+int suppElement(FileImg * File);
+void afficheFile(FileImg *File);
 
 
-int main()
+int main(int argc, char *argv[])
 {
-    printf("TP Files 16.06.2021\n");
+    SDL_Event events;
+    SDL_bool isOpen = SDL_TRUE;
 
+    //Ecran SDL
+    if(SDL_Init(SDL_INIT_EVERYTHING)!=0){
+        SDL_Log("Erreur d'initialisation SDL : %s\n", SDL_GetError());
+        return 1;
+    }else{
+        pWindow=SDL_CreateWindow("An SDL2 window",100,100, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);//sdl_window_shown affiche la fenetre
 
-    ListeChar* File = NULL;
-    File = (ListeChar*)malloc(sizeof(ListeChar));
+        //le renderer est le rendu graphique
 
-    //File *fl_suite;
-
-    char *chr_nom;
-    if ((File = (ListeChar *)malloc(sizeof(ListeChar))) == NULL){
-        return 0;
+        if(pWindow){
+            pRenderer=SDL_CreateRenderer(pWindow, -1,SDL_RENDERER_PRESENTVSYNC);
+        } // rendu dessin, texture, etc.
     }
-    if ((chr_nom = (char *)malloc(50 * sizeof (char))) == NULL){
-        return 0;
-    }
+
+        //Déclaration du file + malloc
+    FileImg* File = NULL;
+    File = (FileImg*)malloc(sizeof(FileImg));
+
     //Initialisation des données
-    initFile(File); //dans l'exemple du tuto suite équivaut à mon FIle ---------
+    initFile(File);
+
+    if(pRenderer){
+        //set color
+        SDL_SetRenderDrawColor(pRenderer,255,255,224,SDL_ALPHA_OPAQUE);
+        //CLear Render
+        SDL_RenderClear(pRenderer);
+
+        pSurface = IMG_Load("./assets/Red.png");
+        pTexture = SDL_CreateTextureFromSurface(pRenderer,pSurface);
+        // Insertion d'un élément dans la file
+        insertElement(File, File->fl_fin, pTexture);
+
+        afficheFile(File);
+        SDL_Delay(1000);
+
+        pSurface = IMG_Load("./assets/Green.png");
+        pTexture = SDL_CreateTextureFromSurface(pRenderer,pSurface);
+        // Insertion d'un élément dans la file
+        insertElement(File, File->fl_fin, pTexture);
+
+        afficheFile(File);
+        SDL_Delay(1000);
+
+        pSurface = IMG_Load("./assets/Blue.png");
+        pTexture = SDL_CreateTextureFromSurface(pRenderer,pSurface);
+        // Insertion d'un élément dans la file
+        insertElement(File, File->fl_fin, pTexture);
+        afficheFile(File);
+        SDL_Delay(1000);
+
+        pSurface = IMG_Load("./assets/Cyan.png");
+        pTexture = SDL_CreateTextureFromSurface(pRenderer,pSurface);
+        // Insertion d'un élément dans la file
+        insertElement(File, File->fl_fin, pTexture);
+
+        afficheFile(File);
+        SDL_Delay(1000);
+
+        pSurface = IMG_Load("./assets/Magenta.png");
+        pTexture = SDL_CreateTextureFromSurface(pRenderer,pSurface);
+        // Insertion d'un élément dans la file
+        insertElement(File, File->fl_fin, pTexture);
+
+        afficheFile(File);
 
 
-    // Insertion d'un élément dans la file
-    printf("Entrez un mot : ");
-    scanf("%s", chr_nom);
-    insertElement(File, File->fl_fin, chr_nom);
-    // Retourner la taille de la file
-    printf("La file (%d/3 elements) \n", File->n_taille);
-    printf("\nDebut de la FILE [ ");
-    //Affichage File
-    afficheFile(File);      //le premier entré sera affiché
-    printf(" ] Fin de la FILE\n\n");
+    }
+
+    while (isOpen)
+    {
+         while (SDL_PollEvent(&events))
+        {
+            switch (events.type)
+            {
+            case SDL_QUIT:
+                isOpen = SDL_FALSE ;
+                break;
+            }
+        }
+
+        //afficheFile(File);
+
+        //Update render
+       // SDL_RenderPresent(pRenderer);
+
+    }
 
 
-    // Insertion 2
-    printf("Entrez un mot : ");
-    scanf("%s", chr_nom);
-    insertElement(File, File->fl_fin, chr_nom);
-    // Retourner la taille de la file
-    printf("La file (%d/3 elements)\n", File->n_taille);
-    printf("\nDebut de la FILE [ ");
-    //Affichage File
-    afficheFile(File);
-    printf(" ] Fin de la FILE\n\n");
+    if ((File = (FileImg *)malloc(sizeof(FileImg))) == NULL){
+        return 0;
+    }
 
-    // Insertion 3
-    printf("Entrez un mot : ");
-    scanf("%s", chr_nom);
-    insertElement(File, File->fl_fin, chr_nom);
-    // Retourner la taille de la file
-    printf("La file (%d/3 elements)\n", File->n_taille);
-    printf("\nDebut de la FILE [ ");
-    //Affichage File
-    afficheFile(File);
-    printf(" ] Fin de la FILE\n\n");
+    // Suppression ecran SDL
 
-    //Consulter le premier element
-    printf ("\nLe premier entré est [ %s ] \n",
-                    file_donnee1(File));
+    if(pRenderer){
+        SDL_DestroyRenderer(pRenderer);
+    }
 
-    // Consutler le dernier element
-    printf ("\nLe dernier entré est [ %s ] \n",
-                    file_donnee2(File));
+    if(pWindow){
+       SDL_DestroyWindow(pWindow);
+    }
 
-    //Enlever un élément de la file
-    printf ("\nLe premier entre est supprime\n");
-    suppElement(File);
-    // Retourner la taille de la file
-    printf("La file (%d/3 elements)\n", File->n_taille);
-    printf("\nDebut de la FILE [ ");
-    //Affichage File
-    afficheFile(File);
-    printf(" ] Fin de la FILE\n\n");
+    //SDL_FreeSurface();
+
+    SDL_Quit(); //détruit tout ce qui constitut la SDL
 
   return 0;
 }
 
 //Initialisation
-void initFile(ListeChar* File){
+void initFile(FileImg* File){
 
     File->fl_debut = NULL;
     File->fl_fin = NULL;
@@ -113,18 +156,16 @@ void initFile(ListeChar* File){
 }
 
 // Insertion d'un élément dans la file
-int insertElement (ListeChar * File , Element * courant, char *chr_donnee){
+int insertElement (FileImg * File , Element * courant, SDL_Texture *pTexture){
     Element* nv_element = NULL;
     nv_element = (Element*)malloc(sizeof(Element));
-    nv_element->chr_donnee = (char *)malloc(50 * sizeof(char));
+
 
     if (nv_element == NULL){
         return 0;
     }
-    if (nv_element->chr_donnee == NULL){
-        return 0;
-    }
-    strcpy (nv_element->chr_donnee, chr_donnee);
+
+    nv_element->pIMG = pTexture;
 
     if(courant == NULL){
         if(File->n_taille == 0){
@@ -132,19 +173,19 @@ int insertElement (ListeChar * File , Element * courant, char *chr_donnee){
         }
          nv_element->p_suivant = File->fl_debut;
          File->fl_debut = nv_element;
-        }else{
-            if(courant->p_suivant == NULL){
-                File->fl_fin = nv_element;
-                }
-                nv_element->p_suivant = courant->p_suivant;
-                courant->p_suivant = nv_element;
-         }
+    }else{
+        if(courant->p_suivant == NULL){
+            File->fl_fin = nv_element;
+        }
+        nv_element->p_suivant = courant->p_suivant;
+        courant->p_suivant = nv_element;
+    }
     File->n_taille++;
     return 1;
 }
 
 // Enlever un élément de la file
-int suppElement(ListeChar * File){
+int suppElement(FileImg * File){
      if(File->n_taille == 0){
         return 0;
      }
@@ -163,14 +204,29 @@ int suppElement(ListeChar * File){
      return 1;
 }
 // Afficher la file
-void afficheFile(ListeChar *File){
+void afficheFile(FileImg *File){
     Element *courant;
     int n_i;
     courant = File->fl_debut;
+    SDL_Rect rect = {0,0,0,0};
 
     for(n_i=0; n_i<File->n_taille; n_i++){
-        printf(" %s ", courant->chr_donnee);
+
+        if(!courant->pIMG){
+            SDL_Log("Unable to set texture: %s", SDL_GetError());
+        }else{
+            rect.x = 100*n_i ;
+            rect.y = 0;
+            rect.w = 100;
+            rect.h = 100;
+            SDL_RenderCopy(pRenderer, courant->pIMG, NULL, &rect);
+        }
         courant = courant->p_suivant;
+
+
+         //Update render
+        SDL_RenderPresent(pRenderer);
+
     }
 }
 
